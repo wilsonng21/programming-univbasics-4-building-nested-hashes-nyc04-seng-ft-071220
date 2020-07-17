c94065cc81f91fead82a7365bad7da78d3422857
Wilson Ng <wilsongch92@gmail.com>
2020-07-17 13:55:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,8 @@ def monopoly_with_third_tier
   {:railroads => {
     :pieces => 4,
     :rent_in_dollars => {
-      :one_piece_owned => 25
+      :one_piece_owned => 25,
+      :two_pieces_owned => 50
     },
     :names => {}
    }
